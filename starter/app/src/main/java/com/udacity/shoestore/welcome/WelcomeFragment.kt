99d88ec6fc946b334d298c99d2bc77fef4a81d37
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-01-19 19:09:11 +0300

    welcome Screen implementation

--- starter/app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.WelcomeFragmentBinding
 
 class WelcomeFragment : Fragment() {
+
+    lateinit var binding: WelcomeFragmentBinding
 
     companion object {
         fun newInstance() = WelcomeFragment()
@@ -19,14 +24,27 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    ): View {
+        binding = WelcomeFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(WelcomeViewModel::class.java)
-        // TODO: Use the ViewModel
+        setUpToolbar()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        binding.navigate.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
+        }
+    }
+
+    private fun setUpToolbar() {
+//        (context as AppCompatActivity).setSupportActionBar(binding.toolbar)
+//        (context as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
 }
